4253236eeaab0b657057990004da484fa37bb950
Tom McLaughlin <tom@codedown.io>
2025-11-25 13:27:09 -0800

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs
+            ];
+
+            shellHook = ''
+              echo "nodehun development environment"
+              echo "node version: $(node --version)"
+            '';
+          };
+        }
+      );
+    };
+}
